45bc57e52d0b81b5a0c2dca68b411776fb532f5a
Takahiro Masui <takahiro.masui@luxiar.com>
2025-12-13 11:27:25 +0900

    feat: implement auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :provider, presence: true
   validates :uid, presence: true, uniqueness: { scope: :provider }
+
+  def self.find_or_create_from_auth_hash(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+    end
+  end
 end
