b0647b5f5bf3a1ce7cd461e6c1bee3893a1f8b0f
Takahiro Masui <takahiro.masui@luxiar.com>
2025-12-19 12:36:16 +0900

    feat: add 'Done' button to desk card view; update user archive card spec for clarity

--- spec/system/user_archive_card_spec.rb
+++ spec/system/user_archive_card_spec.rb
@@ -6,12 +6,26 @@ RSpec.describe 'UserArchiveCard', type: :system do
   describe 'カードArchive機能' do
     include_context 'ログイン済みユーザー'
 
-    it 'ユーザーがカードをDoneできる', :js do
+    it 'ユーザーが投函箱のカードをDoneできる', :js do
       card = create(:card, user: user, title: 'テストカード', content: 'テスト内容', status: :arrived)
       visit dashboard_path
 
       within("##{dom_id(card)}") do
         expect(page).to have_content('テストカード')
+        click_button 'Done'
+      end
+
+      expect(page).to have_no_css("##{dom_id(card)}")
+      expect(card.reload.status).to eq('done')
+      expect(card.reload.completed_at).to be_present
+    end
+
+    it 'ユーザーが机のカードをDoneできる', :js do
+      card = create(:card, user: user, title: '机のカード', content: 'テスト内容', status: :on_desk)
+      visit dashboard_path
+
+      within("##{dom_id(card)}") do
+        expect(page).to have_content('机のカード')
         click_button 'Done'
       end
 
