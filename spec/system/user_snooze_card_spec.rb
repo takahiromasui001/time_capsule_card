3804477a2dd97b56bd9b48fb23f6a6e721e31b0c
Takahiro Masui <takahiro.masui@luxiar.com>
2025-12-25 08:19:02 +0900

    feat: enhance card UI with snooze button and update snooze functionality tests

--- spec/system/user_snooze_card_spec.rb
+++ spec/system/user_snooze_card_spec.rb
@@ -6,25 +6,51 @@ RSpec.describe 'UserSnoozeCard', type: :system do
   describe 'カードSnooze機能' do
     include_context 'ログイン済みユーザー'
 
-    it 'ユーザーがカードをSnoozeできる', :js do
-      card = create(:card, user: user, title: 'テストカード', content: 'テスト内容', status: :arrived)
-      visit dashboard_path
+    let(:card) { create(:card, user:, title: 'テストカード', status:) }
 
-      within("##{dom_id(card)}") do
-        expect(page).to have_content('テストカード')
-        click_button 'Snooze'
+    before { card }
+
+    context 'Inboxからスヌーズする場合' do
+      let(:status) { :arrived }
+
+      it 'カードをSnoozeできる', :js do
+        visit dashboard_path
+
+        within("#inbox_cards ##{dom_id(card)}") do
+          click_button 'Snooze'
+        end
+
+        within("#snooze-form-#{card.id}") do
+          scheduled_time = 1.week.from_now
+          page.execute_script("document.querySelector('input[name=\"scheduled_at\"]').value = '#{scheduled_time.strftime('%Y-%m-%dT%H:%M')}'")
+          click_button '設定'
+        end
+
+        expect(page).to have_no_css("#inbox_cards ##{dom_id(card)}")
+        expect(card.reload.status).to eq('scheduled')
+        expect(card.reload.snoozed_count).to eq(1)
       end
+    end
 
-      within("#snooze-form-#{card.id}") do
-        scheduled_time = 1.week.from_now
-        page.execute_script("document.querySelector('input[name=\"scheduled_at\"]').value = '#{scheduled_time.strftime('%Y-%m-%dT%H:%M')}'")
+    context 'Deskからスヌーズする場合' do
+      let(:status) { :on_desk }
 
-        click_button '設定'
+      it 'カードを再Snoozeできる', :js do
+        visit dashboard_path
+
+        within("#desk_cards ##{dom_id(card)}") do
+          click_button 'Snooze'
+        end
+
+        within("#snooze-form-#{card.id}") do
+          scheduled_time = 1.week.from_now
+          page.execute_script("document.querySelector('input[name=\"scheduled_at\"]').value = '#{scheduled_time.strftime('%Y-%m-%dT%H:%M')}'")
+          click_button '設定'
+        end
+
+        expect(page).to have_no_css("#desk_cards ##{dom_id(card)}")
+        expect(card.reload.status).to eq('scheduled')
       end
-
-      expect(page).to have_no_css("#inbox_cards ##{dom_id(card)}")
-      expect(card.reload.status).to eq('scheduled')
-      expect(card.reload.snoozed_count).to eq(1)
     end
   end
 end
