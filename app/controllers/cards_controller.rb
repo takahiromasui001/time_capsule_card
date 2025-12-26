a2527c1b7cad159983c2c02f9d5a2ab554e80402
Takahiro Masui <takahiro.masui@luxiar.com>
2025-12-26 23:08:03 +0900

    feat: add new card creation functionality and update routes

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,10 @@
 class CardsController < ApplicationController
+  def new
+    @card = current_user.cards.build
+
+    render turbo_stream: turbo_stream.replace('card_form_content', partial: 'cards/form_content', locals: { card: @card })
+  end
+
   def create
     @card = current_user.cards.build(card_params)
     @card.status = :scheduled
