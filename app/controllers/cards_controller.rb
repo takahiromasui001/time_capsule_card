2c3cab0c5e242a03333b4511d7af6018952e248a
Takahiro Masui <takahiro.masui@luxiar.com>
2025-12-26 23:07:27 +0900

    feat: add edit and update actions for cards, enhance form rendering

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,6 +10,24 @@ class CardsController < ApplicationController
     end
   end
 
+  def edit
+    @card = current_user.cards.find(params[:id])
+
+    render turbo_stream: turbo_stream.replace('card_form_content', partial: 'cards/form_content', locals: { card: @card })
+  end
+
+  def update
+    @card = current_user.cards.find(params[:id])
+    @card.assign_attributes(card_params)
+
+    @card.status_scheduled! if @card.will_save_change_to_scheduled_at?
+
+    if @card.save
+      head :ok
+    else
+      render turbo_stream: turbo_stream.replace('card_form_content', partial: 'cards/form_content', locals: { card: @card }), status: :unprocessable_entity
+    end
+  end
   private
 
   def card_params
