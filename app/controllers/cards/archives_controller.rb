0ef5a8472bc1f36020cdab7a7335789f33a3cba6
Takahiro Masui <takahiro.masui@luxiar.com>
2025-12-28 10:46:57 +0900

    feat: implement undo functionality for card actions with toast notifications

--- app/controllers/cards/archives_controller.rb
+++ app/controllers/cards/archives_controller.rb
@@ -3,6 +3,19 @@ class Cards::ArchivesController < ApplicationController
     @card = current_user.cards.find(params[:card_id])
     @card.update!(status: :done, completed_at: Time.current)
 
-    render turbo_stream: turbo_stream.remove(@card)
+    render turbo_stream: [
+      turbo_stream.remove(@card),
+      turbo_stream.append('toast_container', partial: 'shared/undo_toast', locals: { card: @card })
+    ]
+  end
+
+  def destroy
+    @card = current_user.cards.find(params[:card_id])
+    @card.update!(status: :on_desk, completed_at: nil)
+
+    render turbo_stream: [
+      turbo_stream.prepend('desk_cards', partial: 'cards/desk_card', locals: { desk_card: @card }),
+      turbo_stream.remove('undo_toast')
+    ]
   end
 end
