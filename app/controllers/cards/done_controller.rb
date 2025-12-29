8821dac807b72543daf25b0f0bba76effe88cad7
Takahiro Masui <takahiro.masui@luxiar.com>
2025-12-29 11:00:33 +0900

    fix: update pagination limit for done cards from 2 to 30

--- app/controllers/cards/done_controller.rb
+++ app/controllers/cards/done_controller.rb
@@ -1,5 +1,5 @@
 class Cards::DoneController < ApplicationController
   def index
-    @pagy, @cards = pagy(current_user.cards.status_done.order(completed_at: :desc), limit: 2)
+    @pagy, @cards = pagy(current_user.cards.status_done.order(completed_at: :desc), limit: 30)
   end
 end
