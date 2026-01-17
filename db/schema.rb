665d0936a823944f5378dbd0c6671081dc3b49d1
Takahiro Masui <takahiro.masui@luxiar.com>
2026-01-18 08:19:43 +0900

    feat: switch database from PostgreSQL to SQLite3

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2025_12_08_233220) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "cards", force: :cascade do |t|
     t.datetime "completed_at"
     t.text "content"
